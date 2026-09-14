1f610016f3601ff1fd3e50eab0201d4fcf3921a2
Raya Diez de Urdanivia <rayadiez@student.uiwtx.edu>
2026-09-14 17:44:27 -0500

    Combo Animation Implemented

---
+++ Source/Catcher/Private/AbilitySystem/CAbilitySystemNativeTags.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_ABILITY_BASICATTACK);
+
+//this is declaration ONLY
+extern FNativeGameplayTag TAG_ABILITY_BASICATTACT;
+
+
+////////////////////////////////////////////////////////////////////////////
+///                          Explanation                                 //
+///////////////////////////////////////////////////////////////////////////
+///
+//THis is a declaration
+//void Func();
+//This is a definition
+//int age = 10;
+//this is a definition also
+//int year;
+//This is a declaration
+//extern int month;
+
+//You can make a declaration multiple times, but you can only do one definition.
--- Source/Catcher/Private/AbilitySystem/GA_Combo.cpp
+++ Source/Catcher/Private/AbilitySystem/GA_Combo.cpp
@@ -3,6 +3,15 @@
 
 #include "AbilitySystem/GA_Combo.h"
 
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "AbilitySystem/CAbilitySystemNativeTags.h"
+
+UGA_Combo::UGA_Combo()
+{
+	AbilityTags.AddTag(TAG_ABILITY_BASICATTACK);
+	BlockAbilitiesWithTag.AddTag(TAG_ABILITY_BASICATTACK);
+}
+
 void UGA_Combo::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
@@ -12,4 +21,14 @@ void UGA_Combo::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 	}
 
 	UE_LOG(LogTemp, Warning, TEXT("Casting Combo Ability"));
+	
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		UAbilityTask_PlayMontageAndWait* PlayMontageAndWait = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, ComboMontage);
+		
+		PlayMontageAndWait->OnCompleted.AddDynamic(this, &UGA_Combo::K2_EndAbility);
+		PlayMontageAndWait->OnCancelled.AddDynamic(this, &UGA_Combo::K2_EndAbility);
+		
+		PlayMontageAndWait->ReadyForActivation();
+	}
 }
